83d68d6aad87396829a65763973a3a289754de20
KenTasaki <tazaki@startup-technology.com>
2020-03-09 22:55:16 +0900

    [Add] deleteUser

--- handler.js
+++ handler.js
@@ -124,3 +124,30 @@ module.exports.updateUser = async (event, context, callback) => {
     connection.end();
   }
 };
+
+module.exports.deleteUser = async (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  let connection;
+  try {
+    connection = await mysql.createConnection(db_data);
+
+    const id = event.pathParameters.id;
+    const sql = `DELETE from users WHERE id = ?`;
+
+    await connection.query(sql, id);
+    connection.end();
+    callback(null, {
+      statusCode: 200,
+      headers: { "Content-type": "application/json" },
+      body: "User deleted!!"
+    });
+  } catch (err) {
+    connection.end();
+    callback(null, {
+      statusCode: err.statusCode || 500,
+      headers: { "Content-type": "application/json" },
+      body: "Could not create Users: " + err
+    });
+    connection.end();
+  }
+};
