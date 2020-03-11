8dc5d556ba955d2ea92df8ddeed69e5f03df7d3d
KenTasaki <tazaki@startup-technology.com>
2020-03-11 19:23:43 +0900

    [Modify] deleteUser action

--- handler.js
+++ handler.js
@@ -1,12 +1,8 @@
 "use strict";
 
-const mysql = require("mysql2/promise");
-const db_data = require("./config/db");
-
 const getFunc = require("./lib/get");
-const getAllFunc = require("./lib/getAll");
-const createFunc = require("./lib/create");
 const updateFunc = require("./lib/update");
+const deleteFunc = require("./lib/delete");
 
 module.exports.getUser = async (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
@@ -34,27 +30,6 @@ module.exports.updateUser = async (event, context, callback) => {
 
 module.exports.deleteUser = async (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
-  let connection;
-  try {
-    connection = await mysql.createConnection(db_data);
-
-    const id = event.pathParameters.id;
-    const sql = `DELETE from users WHERE id = ?`;
-
-    await connection.query(sql, id);
-    connection.end();
-    callback(null, {
-      statusCode: 200,
-      headers: { "Content-type": "application/json" },
-      body: "User deleted!!"
-    });
-  } catch (err) {
-    connection.end();
-    callback(null, {
-      statusCode: err.statusCode || 500,
-      headers: { "Content-type": "application/json" },
-      body: "Could not create Users: " + err
-    });
-    connection.end();
-  }
+  const response = await deleteFunc(event, "users");
+  callback(null, response);
 };
