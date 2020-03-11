d076113d499e3392ae45393de7e8f4f9bdd236e3
KenTasaki <tazaki@startup-technology.com>
2020-03-11 16:49:15 +0900

    [Modify] getUser action

--- handler.js
+++ handler.js
@@ -3,38 +3,19 @@
 const mysql = require("mysql2/promise");
 const db_data = require("./config/db");
 
+const getFunc = require("./lib/get");
+
 module.exports.getUser = async (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
-  let connection;
-  try {
-    let connection = await mysql.createConnection(db_data);
-    const [result] = await connection.query(
-      "SELECT * FROM users WHERE id = ?",
-      event.pathParameters.id
-    );
-    connection.end();
-    callback(null, {
-      statusCode: 200,
-      headers: { "Content-type": "application/json" },
-      body: JSON.stringify(result[0])
-    });
-  } catch (err) {
-    connection.end();
-    callback(null, {
-      statusCode: err.statusCode || 500,
-      headers: { "Content-type": "application/json" },
-      body: "Could not find User: " + err
-    });
-  } finally {
-    connection.end();
-  }
+  const response = await getFunc(event, "users");
+  callback(null, response);
 };
 
 module.exports.getAllUsers = async (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
   let connection;
   try {
-    let connection = await mysql.createConnection(db_data);
+    connection = await mysql.createConnection(db_data);
     const [result] = await connection.query("SELECT * FROM users");
     connection.end();
     callback(null, {
